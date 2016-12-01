3f07a1e370486d8d23492585260ea71c6e4e9667
Lestrygon <silvere.guislain@hotmail.fr>
2016-12-01 15:29:03 +0100

    Un beau traitement commencé

--- ApplicationWeb/traitement.php
+++ ApplicationWeb/traitement.php
@@ -1,11 +1,27 @@
 <?PHP
-if (isset($_POST['station']) && isset($_POST['latitude']) && isset($_POST['longitude']) && isset($_POST['address']))
+if (isset($_POST['latitude']) && isset($_POST['longitude']) && isset($_POST['station']) && isset($_POST['title']) && isset($_POST['widjet']))
 {
-	$fichier = '';
-	$xml = simplexml_load_file($fichier);
+	$station = $_POST['title'];
+    $latitude = $_POST['latitude'];
+    $longitude = $_POST['longitude'];
+    $adresse = $_POST['address'];
 
-	$station=$_POST['station'];
-    $latitude=$_POST['latitude'];
-    $longitude=$_POST['longitude'];
-    $adresse=$_POST['address'];
+    switch ($_POST['widjet']) {
+    	case 'rer':
+    		$fichier = 'XML/widRER.xml';
+    		$widjet = $_POST['widjet'];
+    		break;
+
+    	case 'meteo':
+    	 	$fichier = 'XML/widMeteo.xml';
+    		$widjet = $_POST['widjet'];
+    	 	break;
+
+    	case 'traffic':
+    		$fichier = 'XML/widTraffic.xml';
+    		$widjet = $_POST['widjet'];
+    		break;
+    }
+    $xml = simplexml_load_file($fichier);
+    print_r($xml);
 }
